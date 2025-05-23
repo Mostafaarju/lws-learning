4fda7dd37d191d81912d0976975f26e99dd90aef
Mostafaarju <mostafaarju@gmail.com>
2025-05-23 03:33:18 +0200

    try to solve problem

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,45 +1,28 @@
 import { useState } from "react";
 
-export default function Form() {
-  const [inputs, setInputs] = useState([
-    {
-      id: 1,
-      label: "input",
-    },
-  ]);
+export default function Picture() {
+  const [isActive, setIsActive] = useState(false);
 
-  const handleAddInput = () => {
-    const nextId = inputs[inputs.length - 1].id + 1;
-    setInputs([
-      ...inputs,
-      {
-        id: nextId,
-        label: "input",
-      },
-    ]);
-  };
+  let backgroundCalssName = "background";
+  let pictureCalssName = "picture";
+
+  if (isActive) {
+    pictureCalssName += "picture--active";
+  } else {
+    backgroundCalssName += "background--active";
+  }
 
   return (
-    <div
-      style={{
-        marginBottom: "5px",
-      }}
-    >
-      {inputs.map((input) => (
-        <div key={input.id}>
-          <input
-            type="text"
-            label={input.label}
-            style={{
-              marginBottom: "5px",
-            }}
-          />
-        </div>
-      ))}
-
-      <div style={{ marginTop: "20px" }}>
-        <button onClick={handleAddInput}>Add Input</button>
-      </div>
+    <div className={backgroundCalssName} onClick={() => setIsActive(false)}>
+      <img
+        onClick={(e) => {
+          e.stopPropagation();
+          setIsActive(true);
+        }}
+        className={pictureCalssName}
+        alt="Rainbow houses in Kampung Pelangi, Indonesia"
+        src="https://i.imgur.com/5qwVYb1.jpeg"
+      />
     </div>
   );
 }
