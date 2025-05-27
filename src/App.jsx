b026f02136a14e7e0dc3c0a6980dfbaaa0261084
Mostafaarju <mostafaarju@gmail.com>
2025-05-27 22:03:43 +0200

    Trying to create Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import Form from "./components/Form";
+import AddTask from "./components/AddTask";
 
 function App() {
   return (
     <>
-      {/* <Pointer /> */}
-      <Form />
+      <h2>Todo App</h2>
+      <AddTask />
     </>
   );
 }
