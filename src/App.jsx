4162843d33dc5791a72ddaec902f688efa807dfb
Mostafaarju <mostafaarju@gmail.com>
2025-05-27 23:18:06 +0200

    Add ui components All file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import "./App.css";
+import { useState } from "react";
 import AddTask from "./components/AddTask";
+import TaskList from "./components/TaskList";
+import { initialTasks } from "./data/tasks";
 
 function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+
+  //handlers
+ 
+
+
   return (
     <>
       <h2>Todo App</h2>
       <AddTask />
+      <TaskList tasks={tasks} />
     </>
   );
 }
