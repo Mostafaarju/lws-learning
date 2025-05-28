34dbc80ccbbb5fde71eb1c364f9793833fe83c25
Mostafaarju <mostafaarju@gmail.com>
2025-05-28 13:13:21 +0200

    Add passing useContext Level in dynamnic level compoents

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,9 +1,11 @@
+import { useContext } from "react";
 import { LevelContext } from "../contexts/levelContext";
 
-export default function Section({level, children }) {
+export default function Section({ children }) {
+  const level = useContext(LevelContext);
   return (
     <section className="section">
-      <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
+      <LevelContext.Provider value={level + 1}>{children}</LevelContext.Provider>
     </section>
   );
 }
