c576a3e071ad818a497a0f5a5491f17bc002972e
Mostafaarju <mostafaarju@gmail.com>
2025-05-28 13:07:05 +0200

    add useContext in this projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import "./App.css";
-import Form from "./components/Form";
+import Heading from "./components/Heading";
+import Section from "./components/Section";
 
-function App() {
+export default function App() {
   return (
-    <>
-      {/* <Pointer /> */}
-      <Form />
-    </>
+    <Section level={1}>
+      <Heading>Title</Heading>
+      <Section level={2}>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Heading>Heading</Heading>
+        <Section level={3}>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Heading>Sub-heading</Heading>
+          <Section level={4}>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+            <Heading>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
   );
 }
-
-export default App;
