d5da2db0d81b8032ecf48d0a57c7b4ed5e81148a
Mostafaarju <mostafaarju@gmail.com>
2025-05-28 02:33:22 +0200

    Add useReduce function and business logic in reduce function with export default, and use switch case

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import AddTask from "./components/AddTask";
 import TaskList from "./components/TaskList";
 import { initialTasks } from "./data/tasks";
+import taskReducer from "./reducers/taskReducer";
 
 function App() {
-  const [tasks, setTasks] = useState(initialTasks);
+  const [tasks, dispatch] = useReducer(taskReducer, initialTasks);
 
   const getNextId = (data) => {
+    if (data.length === 0) return 0;
     const maxId = data.reduce((prev, current) =>
       prev && prev.id > current.id ? prev.id : current.id
     );
@@ -16,35 +18,34 @@ function App() {
   //handlers
   const handleAddTask = (text) => {
     dispatch({
-      type: 'added',
+      type: "added",
       text,
       id: getNextId(tasks),
-    })
-    
+    });
   };
 
   const handleChangeTask = (task) => {
     dispatch({
-      type:'changed',
+      type: "changed",
       task,
-    })
+    });
   };
 
-  const handleDeleteTask = (taskId)=> {
-   dispatch({
-    type: 'deleted',
-    id: taskId,
-   })
-  }
+  const handleDeleteTask = (taskId) => {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  };
 
   return (
     <>
       <h2>Todo App</h2>
       <AddTask onAdd={handleAddTask} />
-      <TaskList 
-      tasks={tasks} 
-      onChangeTask={handleChangeTask} 
-      onDeleteTask={handleDeleteTask}
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
       />
     </>
   );
--- src/reducers/taskReducer.js
+++ src/reducers/taskReducer.js
@@ -1,24 +1,53 @@
 export default function taskReducer(tasks, action) {
-  if (action.type === "added") {
-    return [
-      ...tasks,
-      {
-        id: action.id,
-        text: action.text,
-        done: false,
-      },
-    ];
-  } else if (action.type === "changed") {
-    return tasks.map((t) => {
-      if (t.id === action.task.id) {
-        return action.task;
-      } else {
-        return t;
-      }
-    });
-  } else if (action.type === "deleted") {
-    return tasks.filter((t)=> t.id !== action.id)
-  } else {
-    throw Error(`No action matched with ${action.type}`);
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+
+    default: {
+      throw Error(`No action matched with ${action.type}`);
+    }
   }
+
+  //   if (action.type === "added") {
+  //     return [
+  //       ...tasks,
+  //       {
+  //         id: action.id,
+  //         text: action.text,
+  //         done: false,
+  //       },
+  //     ];
+  //   } else if (action.type === "changed") {
+  //     return tasks.map((t) => {
+  //       if (t.id === action.task.id) {
+  //         return action.task;
+  //       } else {
+  //         return t;
+  //       }
+  //     });
+  //   } else if (action.type === "deleted") {
+  // return tasks.filter((t)=> t.id !== action.id)
+  //   } else {
+  //     throw Error(`No action matched with ${action.type}`);
+  //   }
 }
