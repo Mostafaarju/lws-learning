62f8a6eec3827be4d4cd1ef459ccc05623f48500
Mostafaarju <mostafaarju@gmail.com>
2025-06-26 12:17:22 +0200

    Used another useEffectEvent example

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,9 @@
-// import { useState } from "react";
-import "./App.css";
-import Page from "./components/Page";
-import ShoppingCartContext from "./context/shoppingCartContext";
-import Pointer from "./components/Pointer";
+import Timer from "./components/Timer";
 
-// const items = [
-//   {
-//     id: 1,
-//     title: "Product 1",
-//   },
-// ];
-
-function App() {
-  // const [page, setPage] = useState("/home");
-  // const [cartItems, setCartItems] = useState(items);
-
-  // const handlePageChange = () => {
-  //   setPage("/cart");
-  // };
-
-  // // "editor.fontFamily": "'Fira Code', Monaco",
-
-  // const addItems = () => {
-  //   setCartItems([
-  //     ...cartItems,
-  //     {
-  //       id: 2,
-  //       title: "Product 2",
-  //     },
-  //   ]);
-  // };
-
-  return (
-    <>
-      {/* <ShoppingCartContext.Provider value={cartItems}>
-        <Page url={page} onPageChange={handlePageChange} onAddItems={addItems} />
-      </ShoppingCartContext.Provider> */}
-
-      <Pointer />
-    </>
-  );
+export default function App() {
+    return (
+        <div>
+            <Timer />
+        </div>
+    );
 }
-
-export default App;
--- src/utils/logVisit.js
+++
@@ -1,5 +0,0 @@
-export default function LogVisit(url, numberOfItems) {
-  console.log(
-    `User visited ${url} and he/she has ${numberOfItems} in the cart`
-  );
-}
